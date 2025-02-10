1c359ba79213e9f191616843df5ae76efb02f34f
Pedro Bonfá <pedro.bonfa@estudante.ufjf.br>
2025-02-10 13:45:04 -0300

    insere estado para arma selecionada e imagem da arma, aprensentando-as num select option

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
+import { useState } from 'react';
+import ArmasHades from './infernalArms.json';
 import './App.css'
 
 function App() {
+  const [armaSelecionada, setArmaSelecionada] = useState("Stygian");
+  const [imagemArma, setImagem] = useState(ArmasHades.Armas[armaSelecionada].img);
+  
+  function fazMudanca(event) {
+    const novaArma = event.target.value;
+    setArmaSelecionada(novaArma);
+    setImagem(ArmasHades.Armas[novaArma].img);
+  }
 
   return (
     <>
-     
+      <div>Escolha sua arma</div>
+      <select name="Arma" id="select" onChange={fazMudanca} value={armaSelecionada}>
+        <option value="Stygian">Stygian</option>
+        <option value="Varatha">Varatha</option>
+      </select>
+      <div className='imgArma'>
+        {imagemArma && <img src={imagemArma} alt={armaSelecionada} />}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
