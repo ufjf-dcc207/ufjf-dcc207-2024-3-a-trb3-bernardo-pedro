bfe5f647a5a454bb3c2889fbc7ae8b5d14cf203b
Pedro Bonfá <pedro.bonfa@estudante.ufjf.br>
2025-02-10 16:07:58 -0300

    adiciona o estado etapa e suas condições

--- src/App.tsx
+++ src/App.tsx
@@ -9,48 +9,82 @@ type ArmaKey = keyof typeof ArmasHades.Armas;
 function App() {
   const [armaSelecionada, setArmaSelecionada] = useState<ArmaKey>("Stygian");
   const [imagemArma, setImagemArma] = useState(ArmasHades.Armas[armaSelecionada].img);
-  const [quantidadeTitanBlood, setQuantidadeTitanBlood] = useState(10);
+  const [quantidadeTitanBlood, setQuantidadeTitanBlood] = useState(30);
+  const [etapa, setEtapa] = useState(0);
   
   // Atualiza a arma selecionada
   function fazMudanca(event: React.ChangeEvent<HTMLSelectElement>) {
     const novaArma = event.target.value as ArmaKey;;
     setArmaSelecionada(novaArma);
+    setEtapa(0);
     setImagemArma(ArmasHades.Armas[novaArma].img);
   }
 
   // Avança para a próxima evolução da arma
   function nextStage() {
     const arma = ArmasHades.Armas[armaSelecionada];
-
-    if (!arma) return;
-
     const evolucoes = [arma.img, arma.ev1, arma.ev2, arma.ev3];
     const indexAtual = evolucoes.indexOf(imagemArma);
 
-    // Avança no ciclo ou volta para o início
     if (indexAtual === -1 || indexAtual === evolucoes.length - 1) {
       setImagemArma(arma.img);
+      setEtapa(0);
+      setQuantidadeTitanBlood(30);
     } else {
-      setImagemArma(evolucoes[indexAtual + 1]);
+      if (indexAtual === 0) {
+        if (quantidadeTitanBlood < 5) return;
+        setQuantidadeTitanBlood(quantidadeTitanBlood - 5);
+        setImagemArma(evolucoes[indexAtual + 1]);
+        setEtapa(1);
+      }
+      else if (indexAtual === 1) {
+        if (quantidadeTitanBlood < 15) return;
+        setQuantidadeTitanBlood(quantidadeTitanBlood - 15);
+        setImagemArma(evolucoes[indexAtual + 1]);
+        setEtapa(2);
+      }
+      else if (indexAtual === 2) {
+        if (quantidadeTitanBlood < 16) return;
+        setQuantidadeTitanBlood(quantidadeTitanBlood - 16);
+        setImagemArma(evolucoes[indexAtual + 1]);
+        setEtapa(3);
+      }
     }
   }
   
-  function fazEvo1(){
-    if (quantidadeTitanBlood < 5) return;
-    setImagemArma(ArmasHades.Armas[armaSelecionada].ev1);
-    setQuantidadeTitanBlood(quantidadeTitanBlood - 5);
-  }
+  function fazEvo1() {
+    const arma = ArmasHades.Armas[armaSelecionada]
+    if (etapa === 0 && quantidadeTitanBlood >= 5 ) {
+      setQuantidadeTitanBlood(quantidadeTitanBlood - 5);
+      setEtapa(1);
+      setImagemArma(arma.ev1);
 
-  function fazEvo2(){
-    if (quantidadeTitanBlood < 15) return;
-    setImagemArma(ArmasHades.Armas[armaSelecionada].ev2);
-    setQuantidadeTitanBlood(quantidadeTitanBlood - 15);
+    }else if(etapa>0){
+      setImagemArma(arma.ev1);
+    }
   }
-  
-  function fazEvo3(){
-    if (quantidadeTitanBlood < 16) return;
-    setImagemArma(ArmasHades.Armas[armaSelecionada].ev3);
-    setQuantidadeTitanBlood(quantidadeTitanBlood - 16);
+  function fazEvo2() {
+    const arma = ArmasHades.Armas[armaSelecionada]
+    if (etapa <= 1 && quantidadeTitanBlood >= 15 ) {
+      setQuantidadeTitanBlood(quantidadeTitanBlood - 15);
+      setEtapa(2);
+      setImagemArma(arma.ev2);
+    }
+    else if(etapa>1){
+      setImagemArma(arma.ev2);
+    }
+  }
+  function fazEvo3() {
+    const arma = ArmasHades.Armas[armaSelecionada]
+    if(etapa <= 2 && quantidadeTitanBlood >= 16 ){
+
+      setQuantidadeTitanBlood(quantidadeTitanBlood - 16);
+      setEtapa(3);
+      setImagemArma(arma.ev3);
+    }else if(etapa>2){
+      setImagemArma(arma.ev3);
+    }
+
   }
 
   function carregaTB() {
