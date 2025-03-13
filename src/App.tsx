659bb98b0e142a011bd035fae88aa7b7f9a98b80
Pedro Bonfá <pedro.bonfa@estudante.ufjf.br>
2025-03-13 18:10:54 -0300

    adiciona persistência inicial com useEffect e localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import { useState } from 'react';
-import ArmasHades from './infernalArms.json';
-import './App.css'
-import TitanBlood from './TitanBlood.tsx';
+import { useEffect, useState } from "react";
+import ArmasHades from "./infernalArms.json";
+import "./App.css";
+import TitanBlood from "./TitanBlood.tsx";
 
 //Adiciona tipo chave para o json
 type ArmaKey = keyof typeof ArmasHades.Armas;
 
 function App() {
-  //estado para nome da arma
-  const [armaSelecionada, setArmaSelecionada] = useState<ArmaKey>("Stygian");
-  //estado para imagem da arma
-  const [imagemArma, setImagemArma] = useState(ArmasHades.Armas[armaSelecionada].img);
-  //estado para quantidade de sangue
-  const [quantidadeTitanBlood, setQuantidadeTitanBlood] = useState(30);
-  //estado para controle das evoluções
-  const [etapa, setEtapa] = useState(0);
-  
+  //inicializa o estado com o valor salvo no localStorage, se não há, inicializa com valores iniciais
+  const [armaSelecionada, setArmaSelecionada] = useState<ArmaKey>(
+    (localStorage.getItem("armaSelecionada") as ArmaKey) || "Stygian"
+  );
+  const [quantidadeTitanBlood, setQuantidadeTitanBlood] = useState(
+    parseInt(localStorage.getItem("quantidadeTitanBlood") || "30")
+  );
+  const [etapa, setEtapa] = useState(
+    parseInt(localStorage.getItem("etapa") || "0")
+  );
+  const [imagemArma, setImagemArma] = useState(
+    ArmasHades.Armas[armaSelecionada].img
+  );
+
+  //quando alguns dos estados abaixo é modificado, salva as alterações destes no localStorage
+  useEffect(() => {
+    localStorage.setItem("armaSelecionada", armaSelecionada);
+    localStorage.setItem(
+      "quantidadeTitanBlood",
+      quantidadeTitanBlood.toString()
+    );
+    localStorage.setItem("etapa", etapa.toString());
+  }, [armaSelecionada, quantidadeTitanBlood, etapa]);
+
+  //atualiza interface para os valores corretos
+  useEffect(() => {
+    const arma = ArmasHades.Armas[armaSelecionada];
+    const evolucoes = [arma.img, arma.ev1, arma.ev2, arma.ev3];
+    setImagemArma(evolucoes[etapa]);
+  }, [armaSelecionada, etapa]);
+
   // Atualiza a arma selecionada
   function atualizarArma(event: React.ChangeEvent<HTMLSelectElement>) {
-    const novaArma = event.target.value as ArmaKey;;
+    const novaArma = event.target.value as ArmaKey;
     setArmaSelecionada(novaArma);
     setEtapa(0);
     setImagemArma(ArmasHades.Armas[novaArma].img);
@@ -40,14 +62,12 @@ function App() {
         setQuantidadeTitanBlood(quantidadeTitanBlood - 5);
         setImagemArma(evolucoes[indexAtual + 1]);
         setEtapa(1);
-      }
-      else if (indexAtual === 1) {
+      } else if (indexAtual === 1) {
         if (quantidadeTitanBlood < 15) return;
         setQuantidadeTitanBlood(quantidadeTitanBlood - 15);
         setImagemArma(evolucoes[indexAtual + 1]);
         setEtapa(2);
-      }
-      else if (indexAtual === 2) {
+      } else if (indexAtual === 2) {
         if (quantidadeTitanBlood < 16) return;
         setQuantidadeTitanBlood(quantidadeTitanBlood - 16);
         setImagemArma(evolucoes[indexAtual + 1]);
@@ -55,41 +75,37 @@ function App() {
       }
     }
   }
-  
+
   //Funções para evoluções sob demanda
   function fazEvo1() {
-    const arma = ArmasHades.Armas[armaSelecionada]
-    if (etapa === 0 && quantidadeTitanBlood >= 5 ) {
+    const arma = ArmasHades.Armas[armaSelecionada];
+    if (etapa === 0 && quantidadeTitanBlood >= 5) {
       setQuantidadeTitanBlood(quantidadeTitanBlood - 5);
       setEtapa(1);
       setImagemArma(arma.ev1);
-
-    }else if(etapa>0){
+    } else if (etapa > 0) {
       setImagemArma(arma.ev1);
     }
   }
   function fazEvo2() {
-    const arma = ArmasHades.Armas[armaSelecionada]
-    if (etapa <= 1 && quantidadeTitanBlood >= 15 ) {
+    const arma = ArmasHades.Armas[armaSelecionada];
+    if (etapa <= 1 && quantidadeTitanBlood >= 15) {
       setQuantidadeTitanBlood(quantidadeTitanBlood - 15);
       setEtapa(2);
       setImagemArma(arma.ev2);
-    }
-    else if(etapa>1){
+    } else if (etapa > 1) {
       setImagemArma(arma.ev2);
     }
   }
   function fazEvo3() {
-    const arma = ArmasHades.Armas[armaSelecionada]
-    if(etapa <= 2 && quantidadeTitanBlood >= 16 ){
-
+    const arma = ArmasHades.Armas[armaSelecionada];
+    if (etapa <= 2 && quantidadeTitanBlood >= 16) {
       setQuantidadeTitanBlood(quantidadeTitanBlood - 16);
       setEtapa(3);
       setImagemArma(arma.ev3);
-    }else if(etapa>2){
+    } else if (etapa > 2) {
       setImagemArma(arma.ev3);
     }
-
   }
 
   //Adiciona +1 Titan Blood
@@ -100,7 +116,12 @@ function App() {
   return (
     <>
       <div>Escolha sua arma</div>
-      <select name="Arma" id="select" onChange={atualizarArma} value={armaSelecionada}>
+      <select
+        name="Arma"
+        id="select"
+        onChange={atualizarArma}
+        value={armaSelecionada}
+      >
         <option value="Stygian">Stygian</option>
         <option value="Varatha">Varatha</option>
         <option value="Aegis">Aegis</option>
@@ -108,7 +129,7 @@ function App() {
         <option value="Twin Fists">Twin Fists</option>
         <option value="Exagryph">Exagryph</option>
       </select>
-      <div className='imgArma'>
+      <div className="imgArma">
         {imagemArma && <img src={imagemArma} alt={armaSelecionada} />}
       </div>
       <button onClick={fazEvo1}>Evo1</button>
@@ -117,7 +138,10 @@ function App() {
       <button onClick={carregaTB}>Carregar TB</button>
       <button onClick={nextStage}>Próxima evolução</button>
 
-      <TitanBlood img='src/assets/Adicionais/Titan_Blood.webp' quantidade={quantidadeTitanBlood}></TitanBlood>
+      <TitanBlood
+        img="src/assets/Adicionais/Titan_Blood.webp"
+        quantidade={quantidadeTitanBlood}
+      ></TitanBlood>
     </>
   );
 }
